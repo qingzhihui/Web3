011eaa5967b34b962bdf97b24ba695be16ee35d0
qingzhihui <103401895+qingzhihui@users.noreply.github.com>
2022-04-18 18:09:16 +0800

    Add files via upload

---
+++ Demons5/Nusbun/index.js
@@ -0,0 +1,115 @@
+const { ethers } = require("ethers");
+async function connect() {
+  // 查看window底下元掩码是否存在
+  if (typeof window.ethereum !== "undefined") {
+    await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+  }
+}
+async function execute() {
+  // address 🎉
+  // contract ABI 🎉
+  // function 🎉
+  // node cnnection 🎉
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const abi = [
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "_name",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "addPerson",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      name: "nameToFavoriteNumber",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "people",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "favoriteNumber",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "retrieve",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "store",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ];
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //this is going to get the connected account
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  await contract.store(42);
+}
+
+module.exports = {
+  connect,
+  execute,
+};
---
+++ Demons5/nextjs-web3-react/pages/index.js
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useWeb3React } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import {abi} from "../constants/abi"
+import {ethers} from 'ethers'
+
+const injected = new InjectedConnector();
+
+export default function Home() {
+  const { activate, active, library: provider } = useWeb3React();
+
+  async function connect() {
+    try {
+      await activate(injected);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function execute(){
+    console.log('123')
+    if(active){
+      const signer = provider.getSigner();
+      const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      try {
+        await contract.store(42);
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      {active ? (
+          <>
+          "Connected" <button onClick={() => execute()}>Execute</button>
+          </>
+      ):(
+        <button onClick={() => connect()}>Connect</button>
+      )}
+    </div>
+  );
+}
